a55645abb1b972f03b280a842d22ef76e9d40d79
Latte <cchukwuani@gmail.com>
2021-01-11 23:02:18 -0500

    renamed image and files and updated readme to reflect

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 # Password Generator
 
-## Description 
+## Description
 
-    As an  employee with access to sensitive data. I have created a password generator that will create a password between the length of 8 and 125 characters. You can choose uppercase, lower case letters, numbers, and special characters. 
-    
+    As an  employee with access to sensitive data. I have created a password generator that will 
+    create a password between the length of 8 and 125 characters. You can choose uppercase, 
+    lower case letters, numbers, and special characters.
+
     The password will be generated at random and once you click the generate password button.
 
 ## Usage
+
     To use this generator, follow the prompts at the top of the window:
 
     * Click Generate Password
-    * Input Password Lenghth 
+    * Input Password Lenghth
     * Comfirm Password type (Uppercase, Lower Case, Numbers, & Special Characters)
 
+![Password Generator](assets/Images/Screenshot.png)
 
-![Password Generator](/assets/Images/Screen Shot 2021-01-11 at 10.37.12 PM.png)
+![Password Generator](assets/Images/Screenshot1.png)
 
-![Password Generator](assets/Images/Screen Shot 2021-01-11 at 10.37.38 PM.png)
+![Password Generator](assets/Images/Screenshot2.png)
 
-![Password Generator](assets/Images/Screen Shot 2021-01-11 at 10.38.07 PM.png)
-
-![Password Generator](assets/Images/Screen Shot 2021-01-11 at 10.38.26 PM.png)
+![Password Generator](assets/Images/Screenshot3.png)
---
+++ assets/copy.js
@@ -0,0 +1,93 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Password Criteria Arrays
+
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "=", "-", "_", "'"];
+
+
+
+
+//Function to Start Generating Password
+function generatePassword() {
+  var userPassword = [];
+
+  var userPasswordCriteria = [];
+
+  var passwordLength = parseInt(prompt("provide password length"));//says password length and input length is not defined
+
+
+  // check if the 8 < passwordLength < 128
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("password lenght must be at least 8 characters long and no more than 128 characters");
+
+    return
+  }
+// Makes sure Password length is a number and not string
+  if (isNaN(passwordLength)){
+    
+    alert("Password Length must be a number");
+    
+    return
+  }
+
+  // Confirm the Characters the User would like to use
+
+    var confirmLowerCaseLetters = confirm("Would you like to make your password contain lower case alphabetical characters?");
+    if (confirmLowerCaseLetters){
+      userPasswordCriteria = userPasswordCriteria.concat(lowerCaseLetters);
+     
+    }
+    var confirmUpperCaseLetters = confirm("Would you like to make your password contain upper case alphabetical characters?"); 
+    if (confirmUpperCaseLetters){
+      userPasswordCriteria = userPasswordCriteria.concat(upperCaseLetters);
+    
+  }
+  var confirmNumeric = confirm("Would you like to make your password contain numeric characters?"); 
+  if (confirmNumeric){
+    userPasswordCriteria = userPasswordCriteria.concat(numeric);
+  
+}
+var confirmSpecialCharacters = confirm("Would you like to make your password contain numeric characters?"); 
+if (confirmSpecialCharacters){
+  userPasswordCriteria = userPasswordCriteria.concat(specialCharacters);
+ 
+}
+  // Validation to choose one at least 1 criteria
+  if (confirmLowerCaseLetters  === false && confirmUpperCaseLetters === false && confirmNumeric === false && confirmSpecialCharacters === false){
+    alert("must choose a password criteria")
+  }return userPasswordCriteria; 
+  console.log(userPasswordCriteria);
+
+  
+  //Create Loop for Password
+for (var i = 0; i <= passwordLength; i++){
+//entry holds random index of array
+  var entry = Math.floor(Math.random() * userPasswordCriteria.length);
+
+  userPassword.push(userPasswordCriteria[entry]); 
+}
+
+// console.log("This is line 65", userPassword);
+return userPassword.join('')
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
